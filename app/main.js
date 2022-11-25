100b91ca893accbc3bb3f628e1be8f590f9f6f55
Edu <eduardvf@outlook.com>
2022-11-25 01:02:47 +0100

    first setup

---
+++ app/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, webFrame } = require('electron');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		autoHideMenuBar: true
+	});
+
+	win.loadFile('app/index.html');
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
