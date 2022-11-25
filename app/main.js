d6b22e8c6955e1d05757f4ac438efa597b981dad
Edu <eduardvf@outlook.com>
2022-11-25 21:26:15 +0100

    Add zoom shortcuts

--- app/main.js
+++ app/main.js
@@ -1,13 +1,39 @@
-const { app, BrowserWindow, webFrame } = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+const MIN_WIDTH = 800;
+const MIN_HEIGHT = 600;
 
 const createWindow = () => {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: MIN_WIDTH,
+		height: MIN_HEIGHT,
 		autoHideMenuBar: true
 	});
 
+	win.setMinimumSize(MIN_WIDTH, MIN_HEIGHT);
 	win.loadFile('app/index.html');
+
+	win.webContents.on('dom-ready', (event, input) => {
+		win.webContents.zoomFactor = 1;
+	});
+	win.webContents.on('before-input-event', (event, input) => {
+		if (input.control && input.key === '+') {
+			console.log('zoom in');
+
+			win.webContents.zoomFactor += 0.1;
+
+			event.preventDefault();
+		} else if (input.control && input.key === '-') {
+			console.log('zoom out');
+
+			win.webContents.zoomFactor -= 0.1;
+
+			event.preventDefault();
+		} else if (input.control && input.key.toLowerCase() === 'r') {
+			win.webContents.zoomFactor = 1;
+			event.preventDefault();
+		}
+	});
 };
 
 app.whenReady().then(() => {
